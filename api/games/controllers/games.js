6828a03674b0f5e6d57fb232f53946646db932fe
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-08-06 13:24:08 +0800

    Assign current user on game creation

--- api/games/controllers/games.js
+++ api/games/controllers/games.js
@@ -1,8 +1,24 @@
 'use strict';
 
+const { sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  // Create game with linked user
+  create: async (ctx) => {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.games.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.games.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.games });
+  }
+};
