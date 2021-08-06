70d655620fbd232b179a5bf564d989862005d110
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-08-06 13:25:47 +0800

    Add custom route for Module

---
+++ api/modules/controllers/modules.js
@@ -0,0 +1,47 @@
+module.exports = {
+  all: async () => {
+    const moduleConfig = {
+      start: {
+        path: '/',
+        label: 'Start',
+        tooltip: 'Start',
+        isMenuHidden: false,
+      },
+      news: {
+        path: '/news',
+        label: 'News',
+        tooltip: 'Gaming News',
+        isMenuHidden: false,
+      },
+      reviews: {
+        path: '/reviews',
+        label: 'Reviews',
+        tooltip: 'Game Reviews',
+        isMenuHidden: false,
+      },
+      register: {
+        className: 'menu-register',
+        path: '/auth/register',
+        label: 'Register For Free',
+        tooltip: 'Register For Free',
+        isMenuHidden: true,
+      },
+      login: {
+        className: 'menu-login',
+        path: '/auth/login',
+        label: 'Login',
+        tooltip: 'Login',
+        isMenuHidden: true,
+      },
+      account: {
+        className: 'menu-account',
+        path: '/account',
+        label: 'Account',
+        tooltip: 'Account',
+        isMenuHidden: true,
+      }
+    }
+
+    return JSON.stringify(moduleConfig);
+  }
+};
