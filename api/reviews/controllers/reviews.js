d2a3415e3eb9a101c221ee28a2488ebc81ebf386
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-10-28 20:23:13 +0800

    Add properties and slugify on create

--- api/reviews/controllers/reviews.js
+++ api/reviews/controllers/reviews.js
@@ -1,8 +1,38 @@
 'use strict';
 
+const { sanitizeEntity } = require('strapi-utils');
+const slugify = require('slugify');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  // Create review with linked user
+  create: async (ctx) => {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+
+      if (data.game) {
+        const { game: gameId } = data;
+        const { title } = await strapi.query('games').findOne({ id: gameId });
+        data.slug = slugify(`${title} ${ctx.state.user.id}`, { lower: true });
+      }
+ 
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.reviews.create(data, { files });
+    } else {
+      if (ctx.request.body.game) {
+        const { game: gameId } = ctx.request.body;
+        const { title } = await strapi.query('games').findOne({ id: gameId });
+        ctx.request.body.slug = slugify(`${title} ${ctx.state.user.id}`, { lower: true });
+      }
+
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.reviews.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.reviews });
+  }
+};
--- api/reviews/models/reviews.js
+++ api/reviews/models/reviews.js
@@ -1,8 +1,19 @@
-'use strict';
+const slugify = require('slugify');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      // const { game: gameId } = data;
+      // const {  } = await strapi.query('games').find({ id: gameId });
+      // console.log('foo', foo);
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+    },
+    beforeUpdate: async (data) => {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+    },
+  },
+};
